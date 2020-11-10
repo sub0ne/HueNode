7c396047e117588c0167f9235f6514efdf729c6d
Michael Belz <michael.belz@ilcgroup.com>
2020-11-10 17:16:47 +0100

    - UPnP response from template file

--- src/upnp/UPnPServer.js
+++ src/upnp/UPnPServer.js
@@ -1,9 +1,13 @@
 const dgram = require('dgram');
+const fs = require ('fs');
+const path = require ('path');
 const Logger = require('../Logger.js');
 const SSDPParser = require('./SSDPParser.js');
+const HueConfiguration = require('../hue/configuration/HueConfiguration');
 
-const multicastAddress = "239.255.255.250";
-const port = 1900;
+const MULTICAST_ADDRESS = "239.255.255.250";
+const PORT = 1900;
+const UPNP_RESPONSE_TEMPLATE_FILE = "UPnPResponseTemplate";
 
 class UPnPServer {
 
@@ -19,7 +23,7 @@ class UPnPServer {
     }
 
     async startListening() {
-
+       
         this._socket = dgram.createSocket('udp4');
 
         this._socket.on('message', (message, requestInfo) => {
@@ -27,7 +31,7 @@ class UPnPServer {
             const strMessage = message.toString().trim();
 
             if (strMessage.includes('M-SEARCH * HTTP/1.1') &&
-                strMessage.includes(`HOST: ${multicastAddress}:${port}`) &&
+                strMessage.includes(`HOST: ${MULTICAST_ADDRESS}:${PORT}`) &&
                 strMessage.includes('MAN: "ssdp:discover"') &&
                 strMessage.includes('ST: ssdp:all')) {
 
@@ -38,13 +42,7 @@ class UPnPServer {
 
                 Logger.getLogger().info(`[UPnPServer] Received MX value: ${mxValue}`);
 
-                const strResponse = 'HTTP/1.1 200 OK\r\n' +
-                                    'CACHE-CONTROL: max-age=100\r\n' +
-                                    'EXT:\r\n' +
-                                    'LOCATION: http://192.168.178.34:80/description.xml\r\n' +
-                                    'SERVER: FreeRTOS/6.0.5, UPnP/1.0, IpBridge/0.1\r\n' +
-                                    'ST: upnp:rootdevice\r\n' +
-                                    'USN: uuid:2fa00080-d000-11e1-9b23-001f80007bbe::upnp:rootdevice\r\n';
+                const strResponse = this._getUPnPResponse();
 
                 const response = new Buffer(strResponse); // TODO: use Buffer.from()
 
@@ -66,11 +64,21 @@ class UPnPServer {
 
         });
 
-        this._socket.bind(port, () => {
-            Logger.getLogger().info(`[UPnPServer] Listening to ${multicastAddress}:${port}`);
-            this._socket.addMembership('239.255.255.250');
+        this._socket.bind(PORT, () => {
+            Logger.getLogger().info(`[UPnPServer] Listening to ${MULTICAST_ADDRESS}:${PORT}`);
+            this._socket.addMembership(MULTICAST_ADDRESS);
           });        
 
+    }
+
+    _getUPnPResponseTemplatePath() {
+        return path.join(__dirname, UPNP_RESPONSE_TEMPLATE_FILE);
+    }
+
+    _getUPnPResponse() {
+        const hueConfiguration = new HueConfiguration();
+        let strResponse = fs.readFileSync(this._getUPnPResponseTemplatePath(), "utf8");
+        return strResponse.replace('{uuid}', hueConfiguration.getUUID());
     }
 
 }
