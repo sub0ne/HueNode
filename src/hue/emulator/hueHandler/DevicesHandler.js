003cba9d165cec241267e7f57f93a111aa3d347a
michael <sub.one@outlook.de>
2021-05-02 00:41:17 +0200

    - missing library - extended color light template changed

--- src/hue/emulator/hueHandler/DevicesHandler.js
+++ src/hue/emulator/hueHandler/DevicesHandler.js
@@ -1,5 +1,6 @@
 const BaseHandler = require('./BaseHandler');
 
+const CryptoJS = require('crypto-js');
 const fs = require('fs');
 const path = require('path');
 const DimmableLight = require('../../objects/DimmableLight');
